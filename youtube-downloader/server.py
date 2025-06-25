3866695927dc90c077a2ae51126d11c260db0be5
Suraj Sudhir Thoke <surajthoke06@gmail.com>
2025-06-26 01:08:47 +0530

    Update server.py

--- youtube-downloader/server.py
+++ youtube-downloader/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import yt_dlp
+import os
 
 app = Flask(__name__)
 CORS(app)  # Allow frontend access from browser
@@ -42,5 +43,8 @@ def download_video():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
